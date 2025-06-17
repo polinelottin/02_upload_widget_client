33ad8050c23d32a238df725e1df97a04644332d7
Poline Lottin <poline.lottin@goco.io>
2025-06-17 10:06:28 -0300

    feat: implement dropzone

--- web/src/components/upload-widget-minimized-button.tsx
+++ web/src/components/upload-widget-minimized-button.tsx
@@ -1,18 +1,16 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
 import { Maximize2 } from "lucide-react";
-import { Button } from "./ui/button";
+import { UploadWidgetTitle } from "./upload-widget-title";
 
 export function UploadWidgetMinimizedButton() {
   return (
     <Collapsible.Trigger className="group w-full bg-white/2 py-3 px-5 flex items-center justify-between">
-      <span className="text-sm font-medium">Upload files</span>
+      <UploadWidgetTitle />
 
-      <Button size="icon" className="-mr-2">
-        <Maximize2
-          strokeWidth={1.5}
-          className="size-4 text-zinc-400 group-hover:text-zinc-100"
-        />
-      </Button>
+      <Maximize2
+        strokeWidth={1.5}
+        className="size-4 text-zinc-400 group-hover:text-zinc-100"
+      />
     </Collapsible.Trigger>
   )
 }
