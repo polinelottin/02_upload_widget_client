2cf8ed88e6980acb714f8af21563307841e36da2
Poline Lottin <poline.lottin@goco.io>
2025-06-18 16:12:27 -0300

    feat: add progress bar

--- web/src/components/upload-widget-upload-list.tsx
+++ web/src/components/upload-widget-upload-list.tsx
@@ -1,16 +1,22 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList() {
+  const isUploadingListEmpty = false;
+  
   return (
     <div className="px-3 flex flex-col gap-3">
        <span className="text-xs font-medium">
         Uploaded files <span className="text-zinc-400">(2)</span>
       </span>
 
-      <div className="flex flex-col gap-2">
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-      </div>
+      {isUploadingListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added yet</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <UploadWidgetUploadItem />
+          <UploadWidgetUploadItem />
+        </div>
+      )}
     </div>
   )
 }
